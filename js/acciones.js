3e99959759e0e1f2f57a0bbbb3a9144b4d71262a
Brenda Juana Orta Arias <maestra.brenda.orta@gmail.com>
2014-08-19 14:51:42 -0500

    tres
    Haber si cambio

--- js/acciones.js
+++ js/acciones.js
@@ -3,13 +3,13 @@
 $(document).ready(function(e){ $('#principal').height($('#page').height());
 	document.addEventListener("deviceready",function(){
     	audio=window.plugins.LowLatencyAudio;
-		audio.preloadFX('do', 'audio/DO.mp3', function(){}, function(e){alert ("Error " +e);});
-		audio.preloadFX('re', 'audio/RE.mp3', function(){}, function(e){alert ("Error " +e);});
-		audio.preloadFX('mi', 'audio/MI.mp3', function(){}, function(e){alert ("Error " +e);});
-		audio.preloadFX('fa', 'audio/FA.mp3', function(){}, function(e){alert ("Error " +e);});
-		audio.preloadFX('sol', 'audio/SOL.mp3', function(){}, function(e){alert ("Error " +e);});
-		audio.preloadFX('la', 'audio/LA.mp3', function(){}, function(e){alert ("Error " +e);});
-		audio.preloadFX('si', 'audio/SI.mp3', function(){}, function(e){alert ("Error " +e);});
+		audio.preloadFX('DO','audio/DO.mp3', function(){}, function(e){alert('Error ' + e);});
+		audio.preloadFX('RE','audio/RE.mp3', function(){}, function(e){alert ("Error " +e);});
+		audio.preloadFX('MI','audio/MI.mp3', function(){}, function(e){alert ("Error " +e);});
+		audio.preloadFX('FA','audio/FA.mp3', function(){}, function(e){alert ("Error " +e);});
+		audio.preloadFX('SOL','audio/SOL.mp3', function(){},function(e){alert ("Error " +e);});
+		audio.preloadFX('LA','audio/LA.mp3', function(){}, function(e){alert ("Error " +e);});
+		audio.preloadFX('SI','audio/SI.mp3', function(){}, function(e){alert ("Error " +e);});
 	//se codifica el evento
 	//hacemos una clase para que cambie de color la tecla cuando sea tocada
 	$('.nota').bind('touchstart', function(){
